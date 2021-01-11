6293425ff203ab81a34054ee73f46c65280d781e
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:30:18 +0800

    Update

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function handleMessage(sender_psid, received_message) {
   let response
 
   // Check if the message contains text
-  if (received_message) {
+  if (received_message.text) {
     // Create the payload for a basic text message
     response = {
       text: `You sent the message: "${received_message.text}". Now send me an image!`,
