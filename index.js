350fa1b4b2acc52571ccb8b480c224c39f96ed3c
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:57:29 +0800

    Update

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload
 
   // Set the response based on the postback payload
-  if (payload === 'yest') {
+  if (payload === 'yes') {
     response = { text: 'Thanks!' }
   } else if (payload === 'no') {
     response = { text: 'Oops, try sending another image' }
