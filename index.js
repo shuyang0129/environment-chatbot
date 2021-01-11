8cee29fbbb196024c00dafe9a4264d7bb7335ce9
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:24:01 +0800

    Update

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function handleMessage(sender_psid, received_message) {
       text: `You sent the message: "${received_message.text}". Now send me an image!`,
     }
   } else if (received_message.attachments) {
-    console.log(received_message.attachments)
+    console.log('Attachment', received_message.attachments)
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url
   }
