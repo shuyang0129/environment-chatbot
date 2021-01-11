0abc250125a7f2184f14ab40576d4b37452e53d2
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:54:30 +0800

    Update

--- index.js
+++ index.js
@@ -115,7 +115,21 @@ function handleMessage(sender_psid, received_message) {
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {}
+function handlePostback(sender_psid, received_postback) {
+  let response
+
+  //Get the payload for the postback
+  let payload = received_postback
+
+  // Set the response based on the postback payload
+  if (payload === 'yest') {
+    response = { text: 'Thanks!' }
+  } else if (payload === 'no') {
+    response = { text: 'Oops, try sending another image' }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response)
+}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
