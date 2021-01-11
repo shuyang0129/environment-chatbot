e069d48b15568bba3ec10b6f211766952745dca9
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:44:17 +0800

    Update

--- index.js
+++ index.js
@@ -73,16 +73,17 @@ function handleMessage(sender_psid, received_message) {
   let response
 
   // Check if the message contains text
-  if (received_message.text) {
+  if (received_message) {
     // Create the payload for a basic text message
     response = {
       text: `You sent the message: "${received_message.text}". Now send me an image!`,
     }
-  } else if (received_message.attachments) {
-    console.log('Attachment', received_message.attachments)
-    // Gets the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url
   }
+  // else if (received_message.attachments) {
+  //   console.log('Attachment', received_message.attachments)
+  //   // Gets the URL of the message attachment
+  //   let attachment_url = received_message.attachments[0].payload.url
+  // }
 
   // Sends the response message
   callSendAPI(sender_psid, response)
