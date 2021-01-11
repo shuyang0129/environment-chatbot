ef38d36e952a4dfc3a90ae024f68f7547ba1cb1a
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:42:31 +0800

    Upate

--- index.js
+++ index.js
@@ -72,46 +72,19 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response
 
-  // Checks if the message contains text
-  if (received_message) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
     response = {
-      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
     }
   } else if (received_message.attachments) {
-    // Get the URL of the message attachment
+    console.log('Attachment', received_message.attachments)
+    // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url
-    response = {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [
-            {
-              title: 'Is this the right picture?',
-              subtitle: 'Tap a button to answer.',
-              image_url: attachment_url,
-              buttons: [
-                {
-                  type: 'postback',
-                  title: 'Yes!',
-                  payload: 'yes',
-                },
-                {
-                  type: 'postback',
-                  title: 'No!',
-                  payload: 'no',
-                },
-              ],
-            },
-          ],
-        },
-      },
-    }
   }
 
-  // Send the response message
+  // Sends the response message
   callSendAPI(sender_psid, response)
 }
 
