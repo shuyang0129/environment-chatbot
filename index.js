2dd29ef95bab54ad6bf07d90826d9c1442192d05
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:38:38 +0800

    Update

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function handleMessage(sender_psid, received_message) {
   let response
 
   // Checks if the message contains text
-  if (received_message.text) {
+  if (received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
