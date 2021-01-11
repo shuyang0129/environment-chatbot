17dc4b7af376b9f078320a9f9b0f3fdd4c3b96cb
Shuyang <shuyang0129@gmail.com>
2021-01-11 13:34:48 +0800

    Update

--- index.js
+++ index.js
@@ -82,6 +82,33 @@ function handleMessage(sender_psid, received_message) {
     console.log('Attachment', received_message.attachments)
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url
+    response = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title: 'Is this the right picture?',
+              subtitle: 'Tap a button to answer',
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Yes!',
+                  payload: 'yes',
+                },
+                {
+                  type: 'postback',
+                  title: 'No!',
+                  payload: 'no',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    }
   }
 
   // Sends the response message
